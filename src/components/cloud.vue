63fe4992117188304fa43db281ba025069bdc7ef
luoyu <1093930651@qq.com>
2019-05-16 12:57:17 +0800

    remake

---
+++ src/components/cloud.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="cloud">
+    <div class="satic-area">
+      <div class="dynamic-area1"></div>
+      <div class="dynamic-area2"></div>
+    </div>
+    <div class="content-box" id="content">
+      <svg id="earth_svg"></svg>
+    </div> 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cloud',
+  mounted() {
+  }
+}
+</script>
+
+<style>
+
+</style>
